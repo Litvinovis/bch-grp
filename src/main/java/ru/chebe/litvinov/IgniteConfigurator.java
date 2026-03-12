66b45d08e7558eb9fee04aa7126cf15b8cdd027c
Litvinov Igor <litvinovis102@yandex.ru>
2026-03-12 13:57:12 +0500

    chore: configure Ignite addresses/work-dir via application.yml (#5)
    * chore: configure Ignite addresses via application.yml
    
    * chore: configure Ignite client work-dir via application.yml

--- src/main/java/ru/chebe/litvinov/IgniteConfigurator.java
+++ src/main/java/ru/chebe/litvinov/IgniteConfigurator.java
@@ -6,18 +6,29 @@ import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 
-import java.util.Collections;
+import java.util.List;
 
 public class IgniteConfigurator {
+    private final String localAddress;
+    private final List<String> discoveryAddresses;
+    private final String workDir;
+
+    public IgniteConfigurator(String localAddress, List<String> discoveryAddresses, String workDir) {
+        this.localAddress = localAddress;
+        this.discoveryAddresses = discoveryAddresses;
+        this.workDir = workDir;
+    }
+
     public Ignite getIgnite() {
         IgniteConfiguration cfg = new IgniteConfiguration();
         cfg.setIgniteInstanceName("bchgrp-client");
         cfg.setClientMode(true);
+        cfg.setWorkDirectory(workDir);
 
         TcpDiscoverySpi discoverySpi = new TcpDiscoverySpi();
-        discoverySpi.setLocalAddress("192.168.1.120");
+        discoverySpi.setLocalAddress(localAddress);
         TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
-        ipFinder.setAddresses(Collections.singletonList("192.168.1.120:47650..47659"));
+        ipFinder.setAddresses(discoveryAddresses);
         discoverySpi.setIpFinder(ipFinder);
         cfg.setDiscoverySpi(discoverySpi);
 
